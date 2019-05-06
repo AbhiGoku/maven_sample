dc146da0493b401434ecc9245ed644a3c8156d47
AbhiGoku <abhishekk.l.s1998@gmail.com>
2019-05-06 11:48:30 +0530

    hello

--- app/src/main/java/com/example/HelloServlet.java
+++ app/src/main/java/com/example/HelloServlet.java
@@ -1,6 +1,8 @@
 package com.example;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -32,7 +34,8 @@ public class HelloServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		response.getWriter().write("Hello from servlet");
 	}
 
 	/**
